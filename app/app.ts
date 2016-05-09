42687533debe02953ab7421c05041eb4af8140f6
Wei Zhou <wei2learn@gmail.com>
2016-05-09 13:02:51 -0400

    initial commit

---
+++ app/app.ts
@@ -0,0 +1,52 @@
+import {App, IonicApp, Platform, MenuController} from 'ionic-angular';
+import {StatusBar} from 'ionic-native';
+import {HelloIonicPage} from './pages/hello-ionic/hello-ionic';
+import {ListPage} from './pages/list/list';
+import {InputPage} from './pages/input/input';
+
+
+@App({
+  templateUrl: 'build/app.html',
+  config: {} // http://ionicframework.com/docs/v2/api/config/Config/
+})
+class MyApp2 {
+  // make HelloIonicPage the root (or first) page
+  rootPage: any = HelloIonicPage;
+  pages: Array<{title: string, component: any}>;
+
+  constructor(
+    private app: IonicApp,
+    private platform: Platform,
+    private menu: MenuController
+  ) {
+    
+    
+    
+    
+    this.initializeApp();
+
+    // set our app's pages
+    this.pages = [
+      { title: 'Hello Ionic', component: HelloIonicPage },
+      { title: 'My First List', component: ListPage },
+      { title: 'Input', component: InputPage }
+
+    ];
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+    });
+  }
+
+  openPage(page) {
+    // close the menu when clicking a link from the menu
+    this.menu.close();
+    // navigate to the new page if it is not the current page
+    let nav = this.app.getComponent('nav');
+    nav.setRoot(page.component);
+  }
+}
---
+++ app/pages/input/input.ts
@@ -0,0 +1,12 @@
+import {Page} from 'ionic-angular';
+import {ListPage} from '../list/list';
+
+@Page({
+  templateUrl: './build/pages/input/input.html'
+})
+
+export class InputPage { 
+  
+  ListPage.
+  
+}
